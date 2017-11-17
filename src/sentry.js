7f5460d67ff08b6e627eee539e347755940a8d33
MoOx <m@moox.io>
2017-11-17 19:11:30 +0100

    Make a valid url like for sentry to avoid invariant error with public codebase

--- src/sentry.js
+++ src/sentry.js
@@ -5,5 +5,5 @@ import { Sentry } from "react-native-sentry";
 // Sentry.enableInExpoDevelopment = true;
 
 Sentry.config(
-  "set your own token here - check sentry website"
+  "https://xxx:yyy@sentry.io/zzz"
 ).install();
